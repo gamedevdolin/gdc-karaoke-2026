4f143f2a37c4f7600c2ed77e400bf7e82ce0806f
Adolin <amdolin@gmail.com>
2026-01-26 17:23:26 -0800

    date change

--- app.jsx
+++ app.jsx
@@ -3105,7 +3105,7 @@ function GDCKaraokeApp() {
           {view === 'home' && (
             <div className="presale-banner">
               <span className="presale-badge"><span style={{ whiteSpace: 'nowrap' }}>PRE-SALE IS NOW</span><br/>LIVE!</span>
-              <span className="presale-text">Reserve your room now before the event opens to the general public on February 2nd.</span>
+              <span className="presale-text">Reserve your room now before the event opens to the general public on February 9th.</span>
             </div>
           )}
 
